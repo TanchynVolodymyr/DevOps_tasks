7f27603b7b45e1f51c21461f910308d549d782c5
Volodymyr Tanchyn <31689634+TanchynVolodymyr@users.noreply.github.com>
2021-11-21 21:56:34 +0200

    Add files via upload

---
+++ tanchyn_task1.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+if [[ $EUID -ne 0 ]];
+then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+if [ $# -ne  2 ];
+then
+    echo "need two parametrs for work"
+    exit 1
+fi
+
+if  id -u $1 > /dev/null;
+then
+    echo "..."
+else
+    exit 1
+fi
+
+
+if [ -d $2 ]; then
+    echo "..."
+else
+     echo "you can choose only directory"
+     exit 1
+fi
+
+chown $1 $2
+
+echo "you switch owner $2 to $1"
